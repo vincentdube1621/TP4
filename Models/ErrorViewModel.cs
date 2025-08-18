ad09b719f203e823d03094fac7466d26119403b3
vincentdube1621 <vincent.dube.1621@gmail.com>
2025-08-18 13:22:37 -0400

    Initialisation du projet BibliothequeLIPAJOLI

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BibliothèqueLIPAJOLI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
